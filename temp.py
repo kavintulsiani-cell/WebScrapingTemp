f1248b0493bb46a113c6f777cce6393c430b937a
Kavin Tulsiani <kavintulsiani@gmail.com>
2025-11-09 23:37:43 +0530

    Complete web scraping done, in SQL file

--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
 import requests
 import selectorlib
 from datetime import datetime
+import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com"
 HEADERS = {
@@ -18,11 +19,17 @@ def extract(source):
     value = extractor.extract(source)["temp"]
     return value
 
+connect = sqlite3.connect (r"C:\Users\ADMIN\PycharmProjects\PythonProject10\Temperature.db")
+cursor = connect.cursor()
+
+
 
 scraped = scrape(URL)
 extracted = extract(scraped)
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-with open("temp.txt", "a") as f:
-    f.write(f"{timestamp} - {extracted}\n")
+cursor.execute ("INSERT INTO Temperatures VALUES (?,?)" , (timestamp, extracted))
+connect.commit()
+#with open("temp.txt", "a") as f:
+    #f.write(f"{timestamp} - {extracted}\n")
 
 print (extracted)
